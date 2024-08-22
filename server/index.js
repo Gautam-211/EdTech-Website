b14da25bf92a6e1cb4fb9cfcee195d5ae1cd7542
Gautam Sachdeva <gautamsachdeva20@gmail.com>
2024-08-23 00:25:44 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin:"http://localhost:3000",
+        origin:"https://ed-tech-website-pi.vercel.app/",
         credentials:true
     })
 )
